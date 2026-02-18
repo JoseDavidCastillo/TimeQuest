61634754c529ea5ae4c9fff42c930da8404e5ac5
shiro <jose.castillo-r@mail.escuelaing.edu.co>
2026-02-17 20:28:21 -0500

    start setup

---
+++ TimeQuest_Backend/src/main/java/eci/ieti/TimeQuest_Backend/TimeQuestBackendApplication.java
@@ -0,0 +1,13 @@
+package eci.ieti.TimeQuest_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeQuestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeQuestBackendApplication.class, args);
+	}
+
+}
